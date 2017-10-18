gocardless/bump-core

Add FileFetchers::Elixir::Hex

lib/bump/file_fetchers.rb
@@ -2,6 +2,7 @@
 require "bump/file_fetchers/ruby/bundler"
 require "bump/file_fetchers/python/pip"
 require "bump/file_fetchers/java_script/yarn"
+require "bump/file_fetchers/elixir/hex"
 
 module Bump
   module FileFetchers
@@ -10,6 +11,7 @@ module Bump
       when "bundler" then FileFetchers::Ruby::Bundler
       when "yarn" then FileFetchers::JavaScript::Yarn
       when "pip" then FileFetchers::Python::Pip
+      when "hex_elixir" then FileFetchers::Elixir::Hex
       else raise "Unsupported package_manager #{package_manager}"
       end
     end

lib/bump/file_fetchers/elixir/hex.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+require "bump/file_fetchers/base"
+
+module Bump
+  module FileFetchers
+    module Elixir
+      class Hex < Bump::FileFetchers::Base
+        def self.required_files
+          %w(mix.exs mix.lock)
+        end
+      end
+    end
+  end
+end

spec/bump/file_fetchers/elixir/hex_spec.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+require "bump/file_fetchers/elixir/hex"
+require_relative "../shared_examples_for_file_fetchers"
+
+RSpec.describe Bump::FileFetchers::Elixir::Hex do
+  it_behaves_like "a dependency file fetcher"
+end

lianast/dependabot-core

lib/dependabot/file_fetchers.rb
@@ -6,6 +6,7 @@ require "dependabot/file_fetchers/java_script/yarn"
 require "dependabot/file_fetchers/php/composer"
 require "dependabot/file_fetchers/git/submodules"
 require "dependabot/file_fetchers/docker/docker"
+require "dependabot/file_fetchers/elixir/hex"
 
 module Dependabot
   module FileFetchers
@@ -18,6 +19,7 @@ module Dependabot
       when "composer" then FileFetchers::Php::Composer
       when "submodules" then FileFetchers::Git::Submodules
       when "docker" then FileFetchers::Docker::Docker
+      when "hex" then FileFetchers::Elixir::Hex
       else raise "Unsupported package_manager #{package_manager}"
       end
     end

lib/dependabot/file_fetchers/elixir/hex.rb
@@ -1,0 +1,14 @@
+# frozen_string_literal: true
+require "dependabot/file_fetchers/base"
+
+module Dependabot
+  module FileFetchers
+    module Elixir
+      class Hex < Dependabot::FileFetchers::Base
+        def self.required_files
+          %w(mix.exs mix.lock)
+        end
+      end
+    end
+  end
+end

spec/dependabot/file_fetchers/elixir/hex_spec.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+require "dependabot/file_fetchers/elixir/hex"
+require_relative "../shared_examples_for_file_fetchers"
+
+RSpec.describe Dependabot::FileFetchers::Elixir::Hex do
+  it_behaves_like "a dependency file fetcher"
+end